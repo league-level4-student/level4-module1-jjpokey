6063705c6233bdf887a6a227fba81cbb80011c00
League Student <league@iMac-5.attlocal.net>
2020-02-26 16:59:11 -0800

    Generics

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -4,20 +4,33 @@ package _01_Custom_ArrayList;
 
 public class ArrayList <T>{
 	
+	T[] array;
+	
 	public ArrayList() {
-	}
+		//original
+		array = (T[]) new Object[0]; //important
+		
+		}
+	
 	
 	public T get(int loc) throws IndexOutOfBoundsException {
-		
-		return null;
+
+		return array[loc];
 	}
 	
 	public void add(T val) {
-		
+		//replace code
+		T[] arrayreplace;
+				arrayreplace = (T[]) new Object[array.length + 1];
+				for(int i = 0; i < array.length; i++) {
+					arrayreplace[i] = array[i];
+				}
+				arrayreplace[array.length] = val;
+				array = arrayreplace;
 	}
 	
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
-		
+		//here
 	}
 	
 	public void set(int loc, T val) throws IndexOutOfBoundsException {
